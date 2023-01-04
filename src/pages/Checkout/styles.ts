81764bfd6f769bb31bfba8618953af975ba277d7
Luiz Henrique <lhmorenoti@hotmail.com>
2023-01-04 16:59:01 -0300

    style: Coffee in cart

--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -28,14 +28,6 @@ export const ConfirmationContainer = styled.div`
   border-radius: 6px 44px;
   padding: 2.5rem;
 
-  > hr {
-    border: 0;
-    border-top-width: 1px;
-    border-top-style: solid;
-    border-top-color: ${(props) => props.theme['gray-400']};
-    margin: 1.5rem 0;
-  }
-
   > button {
     margin-top: 1.5rem;
     width: 100%;
@@ -50,59 +42,6 @@ export const ConfirmationContainer = styled.div`
 
   > button:hover {
     background-color: ${(props) => props.theme['yellow-600']};
-  }
-`
-
-export const Coffee = styled.div`
-  display: flex;
-  gap: 1.25rem;
-
-  > img {
-    width: 2.5rem;
-    height: 2.5rem;
-    align-self: center;
-  }
-
-  > div {
-    width: 100%;
-  }
-
-  > div > p {
-    color: ${(props) => props.theme['brown-600']};
-    margin-bottom: 0.5rem;
-  }
-
-  > div > div {
-    display: flex;
-    gap: 0.5rem;
-  }
-
-  > div > div > button {
-    height: 2rem;
-    padding: 0 0.5rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 0.25rem;
-    background-color: ${(props) => props.theme['gray-400']};
-    border-radius: 6px;
-    font-size: 0.75rem;
-    text-transform: uppercase;
-  }
-
-  > div > div > button:hover {
-    background-color: ${(props) => props.theme['gray-500']};
-  }
-
-  > div > div > button svg {
-    width: 1rem;
-    height: 1rem;
-    color: ${(props) => props.theme['purple-500']};
-  }
-
-  > p {
-    min-width: fit-content;
-    font-weight: 700;
   }
 `
 
